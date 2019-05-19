3545dcb231b4bf6dfd81a6405a7490acb224f12f
shotasan <wondermoai@gmail.com>
2019-05-19 22:34:14 +0900

    No.5

--- practice_ruby.rb
+++ practice_ruby.rb
@@ -98,3 +98,32 @@ end
 def panagram?(string)
   ('a'..'z').all? { |x| string.downcase.include? (x) } 
 end
+
+
+# No.5
+# You must create a digital root function.
+# A digital root is the recursive sum of all the digits in a number. Given n, take the sum of the digits of n.
+# If that value has more than one digit, continue reducing in this way until a single-digit number is produced.
+# This is only applicable to the natural numbers.
+
+# digital_root(942)
+# => 9 + 4 + 2
+# => 15 ...
+# => 1 + 5
+# => 6
+
+# digital_root(132189)
+# => 1 + 3 + 2 + 1 + 8 + 9
+# => 24 ...
+# => 2 + 4
+# => 6
+
+# My_answer
+def digital_root(n)
+  n < 10 ? n : digital_root(n.digits.inject(:+))
+end
+
+# Best_answer
+def digital_root(n)
+  n < 10 ? n : digital_root(n / 10 + n % 10)
+end
