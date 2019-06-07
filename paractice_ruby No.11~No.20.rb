6c51bb2a05776933364dcfbe55ac018b4a1ddf76
shotasan <wondermoai@gmail.com>
2019-06-07 14:02:13 +0900

    No.20

--- paractice_ruby No.11~No.20.rb
+++ paractice_ruby No.11~No.20.rb
@@ -282,3 +282,29 @@ end
 def anagrams(word, words)
   words.select { |w| w.chars.sort == word.chars.sort }
 end
+
+
+# No.20
+# Your job is to write a function which increments a string, to create a new string.
+# If the string already ends with a number, the number should be incremented by 1.
+# If the string does not end with a number. the number 1 should be appended to the new string.
+# foo -> foo1
+# foobar23 -> foobar24
+# foo0042 -> foo0043
+# foo9 -> foo10
+# foo099 -> foo100
+
+# My_answer
+def increment_string(input)
+  if input.empty? || input.match(/[a-zA-Z]$/)
+    input + "1"
+  else
+    increments = (input.delete("[^a-zA-Z][\d][^a-zA-Z]").to_i + 1)
+    input.delete("0-9") + sprintf("%0#{input.count('0-9')}d",increments)
+  end
+end
+
+# Best_answer
+def increment_string(input)
+  input.sub(/\d*$/) { |n| n.empty? ? 1 : n.succ }
+end
