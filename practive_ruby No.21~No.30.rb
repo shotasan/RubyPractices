28a3441a321984a0f24fafd42e1db62923ae5083
shotasan <wondermoai@gmail.com>
2019-06-09 23:41:37 +0900

    No.22

--- practive_ruby No.21~No.30.rb
+++ practive_ruby No.21~No.30.rb
@@ -29,3 +29,41 @@ class GroupByDifference
     }
   end
 end
+
+
+# No.22
+# We need a function that can receive a number ann may output in the following order:
+# the amount of multiples
+# the maximum multiple
+# 362 ----> 3, 6, 2, 36, 63, 62, 26, 32, 23, 236, 263, 326, 362, 623, 632
+# But only:3, 6, 36, 63 are multiple of three.
+# Let's see a case the number has a the digit 0 and repeated digits:
+# 6063 ----> 0, 3, 6, 30, 36, 60, 63, 66, 306, 360, 366, 603, 606, 630, 636, 660, 663, 3066, 3606, 3660, 6036, 6063, 6306, 6360, 6603, 6630
+# In this case the multiples of three will be all except 0
+# 6063 ----> 3, 6, 30, 36, 60, 63, 66, 306, 360, 366, 603, 606, 630, 636, 660, 663, 3066, 3606, 3660, 6036, 6063, 6306, 6360, 6603, 6630
+# The cases above for the function:
+# find_mult_3(362) == [4, 63]
+# find_mult_3(6063) == [25, 6630]
+
+# My_answer(false)
+def find_mult_3(num)
+    nums = num.digits
+    i = 1
+    array = []
+    until i > nums.count
+      a = nums.permutation(i).to_a.map{ |num| num.join.to_i }
+      b = a.select{|num| num != 0 && num % 3 == 0}
+      array << b.uniq
+      i += 1
+    end
+    result = array.flatten.uniq
+    return [result.count, result.max]
+end
+
+# Best_answer
+def find_mult_3(num)
+  nums = num.to_s.chars
+  all = (0..num.size).inject(Set.new){|sum,i| sum.merge nums.permutation(i+1).map{|k|k.join.to_i} }
+  div3 = all.select{|l| (l%3) == 0 and !l.zero?}
+  [div3.size, div3.max]
+end
