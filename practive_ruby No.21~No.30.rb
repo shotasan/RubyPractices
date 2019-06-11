e2d0979b3dca721afddca0248140eeb20e03fad8
shotasan <wondermoai@gmail.com>
2019-06-11 14:41:51 +0900

    No.24

--- practive_ruby No.21~No.30.rb
+++ practive_ruby No.21~No.30.rb
@@ -84,3 +84,39 @@ end
 def array_diff(a, b)
   a - b
 end
+
+
+# No.24
+# You are going to be given an array of integers.
+# Your job is to take that array and find an index N where the sum of the integers to the left of N is equal to the sum of the integers to the right of N.
+# If there is no index that would make this happen, return -1.
+
+# My_answer
+def find_even_index(arr)
+  left_side = 0
+  right_side = arr.reduce(:+)
+  
+  arr.each_with_index do |e, ind|
+    right_side -= e
+    return ind if left_side == right_side
+    left_side += e
+  end
+  
+  -1
+end
+
+# Best_answer
+def find_even_index(arr)
+  left_sum = 0
+  right_sum = arr.reduce(:+)
+  
+  arr.each_with_index do |e, ind|
+    right_sum -= e
+    
+    return ind if left_sum == right_sum
+
+    left_sum += e
+  end
+  
+  -1  
+end
