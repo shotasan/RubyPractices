78b288c229617fd21f71953a85f405c1e582e79d
shotasan <wondermoai@gmail.com>
2019-06-16 16:45:36 +0900

    No.29

--- practive_ruby No.21~No.30.rb
+++ practive_ruby No.21~No.30.rb
@@ -324,3 +324,23 @@ def smallest(n)
   end
   result
 end
+
+
+# No.29
+# Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+# You will always get an valid array. And it will be always exactly one letter be missing.
+# The length of the array will always be at least 2.
+# The array will always contain letters in only one case.
+# Example:
+# ['a','b','c','d','f'] -> 'e'
+# ['O','Q','R','S'] -> 'P'
+
+# My_answer
+def find_missing_letter(arr)
+  ([*arr.first..arr.last] - arr).first
+end
+
+# Best_answer
+def find_missing_letter(arr)
+  ((arr.first..arr.last).to_a - arr).first
+end
