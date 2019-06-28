531bb9b5399fa8913a2136f89e6dae512d82526f
shotasan <wondermoai@gmail.com>
2019-06-28 14:16:05 +0900

    No.39

--- practive_ruby No.31~No.40.rb
+++ practive_ruby No.31~No.40.rb
@@ -231,3 +231,32 @@ def revrot(str, sz)
     sum.even? ? digits.reverse : digits.rotate
   end.join
 end
+
+
+# No.39
+# Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. 
+# The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+# Example
+# "abcde" -> 0 # no characters repeats more than once
+# "aabbcde" -> 2 # 'a' and 'b'
+# "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+# "indivisibility" -> 1 # 'i' occurs six times
+# "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+# "aA11" -> 2 # 'a' and '1'
+# "ABBA" -> 2 # 'A' and 'B' each occur twice
+
+# My_answer
+def duplicate_count(text)
+  count = 0
+  text.downcase.chars.uniq.each do |char|
+    count += 1 if text.downcase.chars.count(char) > 1
+  end
+  count
+end
+
+# Best_answer
+def duplicate_count(text)
+  # count {|obj| ... } -> Integer
+  # ブロックを指定した場合は、ブロックを評価して真になった要素の個数を カウントして返します。
+  ('a'..'z').count { |c| text.downcase.count(c) > 1 }
+end
