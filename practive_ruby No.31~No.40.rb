6d6f7e2a7411aca3290a48723dfa113ca237db47
shotasan <wondermoai@gmail.com>
2019-06-29 13:17:57 +0900

    No.40

--- practive_ruby No.31~No.40.rb
+++ practive_ruby No.31~No.40.rb
@@ -260,3 +260,38 @@ def duplicate_count(text)
   # ブロックを指定した場合は、ブロックを評価して真になった要素の個数を カウントして返します。
   ('a'..'z').count { |c| text.downcase.count(c) > 1 }
 end
+
+
+# No.40
+# John has invited some friends. His list is:
+# s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+# Could you make a program that
+# makes this string uppercase
+# gives it sorted in alphabetical order by last name.
+# When the last names are the same, sort them by first name.
+# Last name and first name of a guest come in the result between parentheses separated by a comma.
+# So the result of function meeting(s) will be:
+# "(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
+# It can happen that in two distinct families with the same family name two people have the same first name too.
+
+# My_answer
+def meeting(s)
+  upcase_and_sort(s).map { |last, first| "(" + last + ", " + first + ")" }.join
+end
+
+def upcase_and_sort(s)
+  names = s.split(";").map { |name| name.split(":") }
+  names.map { |first, last| [last.upcase, first.upcase] }.sort
+end
+
+# Best_answer
+def meeting(s)
+  names =
+    s
+      .upcase
+      .split(";")
+      .map { |name| name.split(":") }
+      .sort_by { |name, last| [last, name] }
+      .map { |name, last| "(#{last}, #{name})" }
+      .join
+end
