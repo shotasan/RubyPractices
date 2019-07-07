fcfd4a0e8c0b93d2edfac89be0132021a28d1ef8
shotasan <wondermoai@gmail.com>
2019-07-07 22:48:21 +0900

    No.45

--- practice_ruby No.41~No.50.rb
+++ practice_ruby No.41~No.50.rb
@@ -178,3 +178,53 @@ end
 def createPhoneNumber(numbers)
   "(%d%d%d) %d%d%d-%d%d%d%d" % numbers
 end
+
+
+# No.45
+# The word i18n is a common abbreviation of internationalization in the developer community,
+# used instead of typing the whole word and trying to spell it correctly.
+# Similarly, a11y is an abbreviation of accessibility.
+# Write a function that takes a string and turns any and all "words" (see below) within that string of length 4 or greater into an abbreviation,
+# following these rules:
+# A "word" is a sequence of alphabetical characters.
+# By this definition, any other character like a space or hyphen (eg. "elephant-ride") will split up a series of letters into two words (eg. "elephant" and "ride").
+# The abbreviated version of the word should have the first letter, then the number of removed characters, then the last letter (eg. "elephant ride" => "e6t r2e").
+# Example
+# abbreviate("elephant-rides are really fun!")
+# //          ^^^^^^^^*^^^^^*^^^*^^^^^^*^^^*
+# // words (^):   "elephant" "rides" "are" "really" "fun"
+# //                123456     123     1     1234     1
+# // ignore short words:               X              X
+
+# // abbreviate:    "e6t"     "r3s"  "are"  "r4y"   "fun"
+# // all non-word characters (*) remain in place
+# //                     "-"      " "    " "     " "     "!"
+# === "e6t-r3s are r4y fun!"
+
+# My_answer
+class Abbreviator
+  def self.abbreviate(string)
+    words = self.new.make_words(string)
+    non_words = self.new.make_non_words(string)
+    words.zip(non_words).join
+  end
+
+  def make_words(string)
+    string.scan(/[a-zA-Z]*[^, .-]/).map do |str|
+      str.length > 3 ? str.gsub(/.(.+)./){ str[0] + $1.length.to_s + str[-1] } : str
+    end
+  end
+
+  def make_non_words(string)
+    string.scan(/[, .-]+/)
+  end
+end
+
+# Best_answer
+class Abbreviator
+  def self.abbreviate(string)
+    string.gsub /(\w)(\w+{2})(\w)/ do |word|
+      "#{$1}#{word.length - 2}#{$3}" 
+    end
+  end
+end
