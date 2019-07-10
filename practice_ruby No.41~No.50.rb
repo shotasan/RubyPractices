800606d000c1fa607b5ba9fb7e2dc3008164b8af
shotasan <wondermoai@gmail.com>
2019-07-10 16:25:15 +0900

    No.48

--- practice_ruby No.41~No.50.rb
+++ practice_ruby No.41~No.50.rb
@@ -286,3 +286,28 @@ end
 def score_word(word)
   word.chars.inject(0) { |sum, ch| sum + (ch.ord - 96) }
 end
+
+
+# No.48
+# Given two arrays of strings a1 and a2 return a sorted array r in lexicographical order of the strings of a1 which are substrings of strings of a2.
+# #Example 1: a1 = ["arp", "live", "strong"]
+# a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+# returns ["arp", "live", "strong"]
+# #Example 2: a1 = ["tarp", "mice", "bull"]
+# a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+# returns []
+# Notes:
+# Arrays are written in "general" notation. See "Your Test Cases" for examples in your language.
+# In Shell bash a1 and a2 are strings. The return is a string where words are separated by commas.
+# Beware: r must be without duplicates.
+# Don't mutate the inputs.
+
+# My_answer(False)
+def in_array(a1, a2)
+  a1.find_all { |word| a2.select! { |word2| word2.include?(word) } }
+end
+
+# Best_answer
+def in_array(array1, array2)
+  array1.select{|s| array2.any?{|w| w.include?(s) } }.sort
+end
