9807383979427e67dec5e2aef7daf7c113be3669
shotasan <wondermoai@gmail.com>
2019-07-11 10:33:43 +0900

    No.49

--- practice_ruby No.41~No.50.rb
+++ practice_ruby No.41~No.50.rb
@@ -311,3 +311,36 @@ end
 def in_array(array1, array2)
   array1.select{|s| array2.any?{|w| w.include?(s) } }.sort
 end
+
+
+# No.49
+# Define a function that takes an integer argument and returns logical value true or false depending on if the integer is a prime.
+# Per Wikipedia, a prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+# Example
+# is_prime(1)  /* false */
+# is_prime(2)  /* true  */
+# is_prime(-1) /* false */
+# Assumptions
+# You can assume you will be given an integer input.
+# You can not assume that the integer will be only positive. You may be given negative numbers as well (or 0).
+# There are no fancy optimizations required, but still the most trivial solutions might time out.
+# Try to find a solution which does not loop all the way up to n.
+
+# My_answer
+def isPrime(num)
+  return false if num < 2
+
+  sqrt_num = Math.sqrt(num).floor
+  2.upto(sqrt_num){|i|
+    if num % i == 0
+      return false  # 割り切れたら素数でない
+    end
+  }
+  return true
+end
+
+# Best_answer
+def isPrime(num)
+  return false if num < 2
+  (2...num).to_a.select{|x| num%x == 0}.compact.empty?
+end
