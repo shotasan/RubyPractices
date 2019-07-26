efa4aefda8f7ec67298dd6b952e9d865a6f41df5
shotasan <wondermoai@gmail.com>
2019-07-26 09:33:00 +0900

    No.62

--- practice_ruby No.61~No.70.rb
+++ practice_ruby No.61~No.70.rb
@@ -30,3 +30,32 @@ end
 def sum_dig_pow(a, b)
   (a...b).select { |n| n == n.to_s.chars.map.with_index(1) { |e, i| e.to_i ** i }.reduce(:+) }
 end
+
+
+# No.62
+# A string is considered to be in title case if each word in the string is either
+# (a) capitalised (that is, only the first letter of the word is in upper case) or
+# (b) considered to be an exception and put entirely into lower case unless it is the first word, which is always capitalised.
+# Write a function that will convert a string into title case, given an optional list of exceptions (minor words).
+# The list of minor words will be given as a string with each word separated by a space.
+# Your function should ignore the case of the minor words string -- it should behave in the same way even if the case of the minor word string is changed.
+# First argument (required): the original string to be converted.
+# Second argument (optional): space-delimited list of minor words that must always be lowercase except for the first word in the string.
+# ###Example
+# title_case('a clash of KINGS', 'a an the of') # should return: 'A Clash of Kings'
+# title_case('THE WIND IN THE WILLOWS', 'The In') # should return: 'The Wind in the Willows'
+# title_case('the quick brown fox') # should return: 'The Quick Brown Fox'
+
+# My_answer
+def title_case(title, minor_words = '')
+  return '' if title.empty?
+  
+  title.downcase.split(' ').map.with_index do |word, idx|
+    minor_words.downcase.split(' ').include?(word) && idx != 0 ? word : word.capitalize
+  end.join(' ')
+end
+
+# Best_answer
+def title_case(title, minor_words = '')
+  title.capitalize.split().map{|a| minor_words.downcase.split().include?(a) ? a : a.capitalize}.join(' ')
+end
