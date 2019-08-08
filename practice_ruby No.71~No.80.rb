bb8994280a01c6b8a357e4f8d229ce5cd324e41f
shotasan <wondermoai@gmail.com>
2019-08-08 12:09:56 +0900

    No.72

--- practice_ruby No.71~No.80.rb
+++ practice_ruby No.71~No.80.rb
@@ -14,3 +14,35 @@ regex = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{6,}+\z/
 
 # Best_answer
 regex = /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])^[a-zA-Z0-9]{6,}$/
+
+
+# No.72
+# Write a function named first_non_repeating_letter that takes a string input,
+# and returns the first character that is not repeated anywhere in the string.
+# For example, if given the input 'stress', the function should return 't',
+# since the letter t only occurs once in the string, and occurs first in the string.
+# As an added challenge, upper- and lowercase letters are considered the same character,
+# but the function should return the correct case for the initial letter. For example, the input 'sTreSS' should return 'T'.
+# If a string contains all repeating characters, it should return an empty string ("") or None -- see sample tests.
+# first_non_repeating_letter('a'), 'a'
+# first_non_repeating_letter('stress'), 't'
+# first_non_repeating_letter('moonmen'), 'e'
+
+# My_answer(false)
+def  first_non_repeating_letter(s)
+  return "" if s.empty?
+  return s if s.length == 1
+  table = Hash.new(0)
+  s.downcase.scan(/./){ |m| table[m] += 1}
+  if table.values.uniq.length == 1
+    return ""
+  else
+    min_word = table.key(table.values.min)
+    s.scan(/#{min_word}/i)[0]
+  end
+end
+
+# Best_answer
+def  first_non_repeating_letter(s) 
+  s.chars.find {|i| s.downcase.count(i)==1 || s.upcase.count(i)==1} || ""
+end
