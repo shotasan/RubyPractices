09a1659ab86df1921e6a8d0c062c6214e8dbbb6e
shotasan <wondermoai@gmail.com>
2019-08-26 22:48:19 +0900

    No.79

--- practice_ruby No.71~No.80.rb
+++ practice_ruby No.71~No.80.rb
@@ -340,3 +340,31 @@ def perimeter(n)
     sum + a
   end * 4
 end
+
+
+# No.79
+# The marketing team is spending way too much time typing in hashtags.
+# Let's help them with our own Hashtag Generator!
+# Here's the deal:
+# It must start with a hashtag (#).
+# All words must have their first letter capitalized.
+# If the final result is longer than 140 chars it must return false.
+# If the input or the result is an empty string it must return false.
+# Examples
+# " Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
+# "    Hello     World   "                  =>  "#HelloWorld"
+# ""                                        =>  false
+
+# My_answer
+def generateHashtag(str)
+  return false if str.strip.empty?
+
+  result = str.split(" ").map(&:capitalize).join
+  result.length < 140 ? "#" + result : false 
+end
+
+# Best_answer
+def generateHashtag(str)
+  str = "#" << str.split.map(&:capitalize).join
+  str.size <= 140 && str.size > 1 ? str : false
+end
