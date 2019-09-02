d8583094478296ff1f3243a06813d5175a6bbca5
shotasan <wondermoai@gmail.com>
2019-09-02 13:10:28 +0900

    No.83

--- practice_ruby No.81~No.90.rb
+++ practice_ruby No.81~No.90.rb
@@ -53,3 +53,32 @@ def going(n)
   end
   return (res*1000000).floor/1000000.0
 end
+
+
+# No.83
+# Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+# Rules for a smiling face:
+# -Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+# -A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+# -Every smiling face must have a smiling mouth that should be marked with either ) or D.
+# No additional characters are allowed except for those mentioned.
+# Valid smiley face examples:
+# :) :D ;-D :~)
+# Invalid smiley faces:
+# ;( :> :} :] 
+# Example cases:
+# countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
+# countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
+# countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
+# Note: In case of an empty array return 0. You will not be tested with invalid input (input will always be an array). 
+# Order of the face (eyes, nose, mouth) elements will always be the same
+
+# My_answer
+def count_smileys(arr)
+  arr.map{|item| item.match(/[:;][-~]*[)D]/)}.compact.count
+end
+
+# Best_answer
+def count_smileys(arr)
+  arr.count { |e| e =~ /(:|;){1}(-|~)?(\)|D)/ }
+end
