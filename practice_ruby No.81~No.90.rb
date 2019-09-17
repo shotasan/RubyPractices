b9acaa50f0e152d0da02f2efe990953caf0a608b
shotasan <wondermoai@gmail.com>
2019-09-17 09:45:03 +0900

    No.86

--- practice_ruby No.81~No.90.rb
+++ practice_ruby No.81~No.90.rb
@@ -101,3 +101,98 @@ end
 def rot13(string)
   string.tr("A-Za-z", "N-ZA-Mn-za-m")
 end
+
+
+# No.85
+# Your task in order to complete this Kata is to write a function which formats a duration, given as a number of seconds, in a human-friendly way.
+# The function must accept a non-negative integer. If it is zero, it just returns "now". Otherwise, the duration is expressed as a combination of years, days, hours, minutes and seconds.
+# It is much easier to understand with an example:
+# format_duration(62)    # returns "1 minute and 2 seconds"
+# format_duration(3662)  # returns "1 hour, 1 minute and 2 seconds"
+# For the purpose of this Kata, a year is 365 days and a day is 24 hours.
+# Note that spaces are important.
+# Detailed rules
+# The resulting expression is made of components like 4 seconds, 1 year, etc. In general, a positive integer and one of the valid units of time, separated by a space.
+# The unit of time is used in plural if the integer is greater than 1.
+# The components are separated by a comma and a space (", "). Except the last component, which is separated by " and ", just like it would be written in English.
+# A more significant units of time will occur before than a least significant one. Therefore, 1 second and 1 year is not correct, but 1 year and 1 second is.
+# Different components have different unit of times. So there is not repeated units like in 5 seconds and 1 second.
+# A component will not appear at all if its value happens to be zero. Hence, 1 minute and 0 seconds is not valid, but it should be just 1 minute.
+# A unit of time must be used "as much as possible". It means that the function should not return 61 seconds, but 1 minute and 1 second instead.
+# Formally, the duration specified by of a component must not be greater than any valid more significant unit of time.
+
+# My_answer(false)
+def format_duration(seconds)
+  check(seconds, result=[])
+  result
+end
+
+def check(seconds, result)
+  if seconds >= 60
+    quotient_surplus = seconds.divmod(60)
+    result.unshift(quotient_surplus[1])
+    check(quotient_surplus[0], result)
+  else
+    result.unshift(seconds)
+  end
+
+  if result.length < 3
+    until result.length == 3
+      result.unshift(0)
+    end
+  end
+  result
+end
+
+# Best_answer
+def format_duration(total)
+  if total == 0
+    "now"
+  else
+    duration = {
+      year:   total / (60 * 60 * 24 * 365),
+      day:    total / (60 * 60 * 24) % 365,
+      hour:   total / (60 * 60) % 24,
+      minute: total / 60 % 60,
+      second: total % 60
+    }
+  
+    @output = []
+  
+    duration.each do |key, value|
+      if value > 0
+        @output << "#{value} #{key}"
+        @output.last << "s" if value != 1
+      end
+    end
+  
+    @output.join(', ').gsub(/\,\s(?=\d+\s\w+$)/, " and ")
+  end
+end
+
+
+# NO.86
+# Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
+# array = [[1,2,3],
+#          [4,5,6],
+#          [7,8,9]]
+# snail(array) #=> [1,2,3,6,9,8,7,4,5]
+# For better understanding, please follow the numbers of the next array consecutively:
+# array = [[1,2,3],
+#          [8,9,4],
+#          [7,6,5]]
+# snail(array) #=> [1,2,3,4,5,6,7,8,9]
+# NOTE: The idea is not sort the elements from the lowest value to the highest; the idea is to traverse the 2-d array in a clockwise snailshell pattern.
+# NOTE 2: The 0x0 (empty matrix) is represented as [[]]
+
+# Best_answer
+# 横、縦、横と組み合わせを作成し、順に結合していく
+# transpose -> Array
+# 自身を行列と見立てて、行列の転置(行と列の入れ換え)を行いま す。転置した配列を生成して返します。空の配列に対しては空の配列を生 成して返します。
+def snail(array)
+  if array.empty?
+     []
+  else
+    array.shift + snail(array.transpose.reverse)
+  end
+end
