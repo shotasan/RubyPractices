710d1f30792f4594144b25b9bab1411075f19ce2
shotasan <wondermoai@gmail.com>
2019-10-25 11:40:44 +0900

    No.90

--- practice_ruby No.81~No.90.rb
+++ practice_ruby No.81~No.90.rb
@@ -257,3 +257,66 @@ def choose_best_sum(t, k, ls)
       .select{|sum| sum <= t}
       .max
 end
+
+
+# No.89
+# Description:
+# A child is playing with a ball on the nth floor of a tall building. The height of this floor, h, is known.
+# He drops the ball out of the window. The ball bounces (for example), to two-thirds of its height (a bounce of 0.66).
+# His mother looks out of a window 1.5 meters from the ground.
+# How many times will the mother see the ball pass in front of her window (including when it's falling and bouncing?
+# Three conditions must be met for a valid experiment:
+# Float parameter "h" in meters must be greater than 0
+# Float parameter "bounce" must be greater than 0 and less than 1
+# Float parameter "window" must be less than h.
+# If all three conditions above are fulfilled, return a positive integer, otherwise return -1.
+# Note:
+# The ball can only be seen if the height of the rebounding ball is strictly greater than the window parameter.
+# Example:
+# - h = 3, bounce = 0.66, window = 1.5, result is 3
+# - h = 3, bounce = 1, window = 1.5, result is -1 
+# (Condition 2) not fulfilled).
+
+# My_answer
+def bouncingBall(h, bounce, window)
+  return -1 if h <= 0 && bounce >= 1
+  count = 1
+  h *= bounce
+  while(h > window)
+    count += 2
+    h *= bounce
+  end
+  count
+end
+
+# Best_answer
+def bouncingBall(h, bounce, window)
+  bounce < 0 || bounce >= 1 || h <= window || window < 0 ? -1 : bouncingBall(h * bounce, bounce, window) + 2
+end
+
+
+# No.90
+# Description:
+# Pete likes to bake some cakes. He has some recipes and ingredients. 
+# Unfortunately he is not good in maths. Can you help him to find out, how many cakes he could bake considering his recipes?
+# Write a function cakes(), which takes the recipe (object) and the available ingredients (also an object) and returns the maximum number of cakes Pete can bake (integer).
+# For simplicity there are no units for the amounts (e.g. 1 lb of flour or 200 g of sugar are simply 1 or 200).
+# Ingredients that are not present in the objects, can be considered as 0.
+# Examples:
+# // must return 2
+# cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}); 
+# // must return 0
+# cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000}); 
+
+# My_answer
+def cakes(recipe, available)
+  if (recipe.keys - available.keys).all?{|k| !recipe.include?(k)}
+    return recipe.keys.map{ |k| available[k] / recipe[k] }.min
+  end
+  0
+end
+
+# Best_answer
+def cakes(recipe, available)
+ recipe.keys.map{|k| available[k].to_f/recipe[k] rescue 0}.min.floor
+end
