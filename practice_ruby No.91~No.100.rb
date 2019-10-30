91c37a514ce541f5c8719eb2a98cd0dec8df6fb7
shotasan <wondermoai@gmail.com>
2019-10-30 09:48:49 +0900

    No.94

--- practice_ruby No.91~No.100.rb
+++ practice_ruby No.91~No.100.rb
@@ -104,3 +104,24 @@ final = []
   end
   return final + first + second
 end
+
+
+# No.94
+# Description:
+# Write a function named first_non_repeating_letter that takes a string input,
+# and returns the first character that is not repeated anywhere in the string.
+# For example, if given the input 'stress', the function should return 't',
+# since the letter t only occurs once in the string, and occurs first in the string.
+# As an added challenge, upper- and lowercase letters are considered the same character,
+# but the function should return the correct case for the initial letter. For example, the input 'sTreSS' should return 'T'.
+# If a string contains all repeating characters, it should return an empty string ("") or None -- see sample tests.
+
+# My_answer
+def first_non_repeating_letter(s)
+  s.chars.find{|char| s.downcase.count(char.downcase) <= 1} || ''
+end
+
+# Best_answer
+def first_non_repeating_letter(s) 
+  s.chars.find {|i| s.downcase.count(i)==1 || s.upcase.count(i)==1} || ""
+end
